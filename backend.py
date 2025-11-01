5eeaa4c4c7f93da9c40fb16ceee8c8f452a0a156
selsabila-gtm <selsabila.guettaftemam@ensia.edu.dz>
2025-11-01 19:06:55 +0100

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,40 @@
+from flask import Flask
+import json
+import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/news.all.get')
+def get_news_all_articles():
+	data=[]
+	with open('news_data.json', 'r') as file:
+		data = json.load(file)
+	app.logger.debug('_________________Hello '+str(data))
+	return json.dumps(data)
+
+
+@app.route('/news.categories.get')
+def get_news_categories():
+	time_now_str=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	data={
+     	'title':'List of Categories',
+      	'time':time_now_str,
+       	'categories':[
+            	{'id':1,'name':'Sports'},
+             	{'id':2,'name':'Politics'},
+              	{'id':3,'name':'Education'}]
+       }
+	return json.dumps(data)
+
+
+@app.route('/')
+def index():
+	return 'Welcome ENSIA Studentssss from Flask!'
+
+if __name__ == "__main__":
+	import os
+	port = int(os.environ.get("PORT", 8080))
+	app.run(host='0.0.0.0', port=port)
+
+
